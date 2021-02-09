b48b6e3176d086b9e1b6702511af1bba390a5abb
lucascabraldeve <lucas_cabral.deve@outlook.com>
2021-02-09 19:36:05 -0300

    Toolbar configurada.

--- app/src/main/java/com/lucascabral/mysubscribers/MainActivity.kt
+++ app/src/main/java/com/lucascabral/mysubscribers/MainActivity.kt
@@ -2,10 +2,19 @@ package com.lucascabral.mysubscribers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        toolbarMain.setupWithNavController(navController, appBarConfiguration)
     }
 }
